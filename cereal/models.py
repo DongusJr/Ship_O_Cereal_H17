0d60dd7d96327ed59386db15c82d0949e9e8f0ab
Guðjón Karl Ólafsson <gudjono19@ru.is>
2021-05-03 12:33:48 +0000

    models draft

--- cereal/models.py
+++ cereal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ZipCodes(models.Model):
+    zip = models.IntegerField()
+    city_name = models.CharField(max_length=64)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+    email = models.CharField(max_length=256, unique=True)
+    zip = models.ForeignKey(ZipCode)
+    address = models.CharField(max_length=128)
