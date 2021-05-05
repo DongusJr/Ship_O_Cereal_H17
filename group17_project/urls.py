dc8c33a3b824d5eac1e7d21cafab00d0e4019171
Dongus <bjarkim19@ru.is>
2021-05-05 11:57:36 +0000

    Lets go, login, register and logout available for user, and it is saved to the db

--- group17_project/urls.py
+++ group17_project/urls.py
@@ -21,5 +21,6 @@ import products.views
 urlpatterns = [
     path('', products.views.get_product_by_tags, name='landing_page'),
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('user/', include('users.urls'))
 ]
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from products.models import Products
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ZipCodes(models.Model):
@@ -34,10 +34,8 @@ class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
 
-class User(models.Model):
-    session_id = models.IntegerField()
-    username = models.CharField(max_length=128)
-    password = models.CharField(max_length=128)
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=256, unique=True)
     zip = models.ForeignKey(ZipCodes, on_delete=models.CASCADE)
     address = models.CharField(max_length=128)
