7723cc795fca67abcc45111ba2330417a3296acc
Dongus <bjarkim19@ru.is>
2021-05-06 11:22:20 +0000

    Making profile page

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from users import views
 urlpatterns = [
     path('account', views.login_register, name='login_register'),
     path('logout', LogoutView.as_view(next_page='login_register'), name='logout'),
-    path('profile', views.Profile.as_view(), name='profile')
+    path('profile', views.UserProfile.as_view(), name='profile')
 ]
