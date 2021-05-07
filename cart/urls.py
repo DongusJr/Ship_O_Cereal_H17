c1f2b08fe63fa07fe9f47c55dc3a850ae63190c2
Guðjón Karl Ólafsson <gudjono19@ru.is>
2021-05-07 10:30:18 +0000

    losing my shit

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart', views.CartView.as_view(), name='cart_page'),
-    path('cart', views.CartItems.as_view(), name='cart_items')
+    path('cart', views.CartView.as_view(), name='cart_page')
 ]
