cfff7201af63bc6f79438fa1b73865964cbe479c
Ingólfur Orri <ingolfurorri@gmail.com>
2021-05-10 14:36:42 +0000

    Email newsletter site done

--- aboutus/views.py
+++ aboutus/views.py
@@ -3,10 +3,11 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class AboutUs(TemplateView):
-    template_name = 'proto_aboutus/proto_aboutus.html'
+    template_name = 'email_nws.html'
     data = {}
 
     def get(self, request, *args, **kwargs):
         self.data['aboutus'] = 'lorem ipsum'
         self.data['terms'] = 'lorem ipsum'
         return render(request, self.template_name, self.data)
+
